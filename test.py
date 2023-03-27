86c79220375b80f3322a7716a356b2fe627c6c13
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-27 18:35:44 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://www.google.com")
+
+search_box = driver.find_element_by_css_selector("input[name='q']")
+print("Suchfeld gefunden:", search_box)
+
+driver.quit()

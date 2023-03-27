6f46f062092f2b6816a766d852a5692eeed1e46b
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-28 00:28:05 +0200

    Update unfollow.py
    add cookie xpath

--- unfollow.py
+++ unfollow.py
@@ -10,8 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
- 
 options = Options()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
